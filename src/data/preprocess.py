4f77c040474e6a071eb12a290a4da2ab6cbf4c1e
DozieSixtus <doziesixtus@gmail.com>
2024-06-21 22:17:46 +0100

    Add count and hashing vectorizers

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -6,8 +6,10 @@ import numpy as np
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
 from tqdm import tqdm
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,roc_curve,classification_report,f1_score
+from scikitplot.metrics import plot_confusion_matrix
 
 dataset = pd.read_csv(r".\data\amazon_electronics_review.csv", sep='\t', index_col=[0])
 dataset.sample(frac=1).head(5) # shuffle the df and pick first 5
@@ -15,7 +17,9 @@ dataset = dataset.iloc[:34000]
 dataset['Label'] = dataset['overall'].apply(lambda x: 'negative' if x<3 else ('neutral' if x==3 else 'positive'))
 
 train, test = train_test_split(dataset, test_size=0.2)
+
 # Create feature vectors
+# TFIDF vectorizer
 vectorizer = TfidfVectorizer(min_df = 0.0,
                              max_df = 0.9,
                              sublinear_tf = True,
@@ -24,7 +28,7 @@ arr = []
 def chunkIterator(df):
     for data_point in df['reviewText'].values.astype('U'):
         yield data_point
-arr = vectorizer.fit_transform(chunkIterator(train))
+#arr = vectorizer.fit_transform(chunkIterator(train))
 train_vectors = vectorizer.fit_transform(train['reviewText'].values.astype('U'))
 test_vectors = vectorizer.transform(test['reviewText'].values.astype('U'))
 
@@ -53,6 +57,115 @@ review = """This product doesn't work correctly"""
 review_vector = vectorizer.transform([review]) # vectorizing
 print(classifier_linear.predict(review_vector))
 
+#rcParams['figure.figsize'] = 10,5
+plot_confusion_matrix(prediction_linear,test['Label'])
+acc_score = accuracy_score(prediction_linear,test['Label'])
+pre_score = precision_score(prediction_linear,test['Label'], average=None)
+rec_score = recall_score(prediction_linear,test['Label'],average=None)
+f1score = f1_score(prediction_linear,test['Label'],average=None)
+print('Accuracy_score: ',acc_score)
+print('Precision_score: ',pre_score)
+print('Recall_score: ',rec_score)
+print('F1 score', f1score)
+print("-"*50)
+cr = classification_report(prediction_linear,test['Label'])
+print(cr)
+
+# Count vectorizer
+vectorizer = CountVectorizer(min_df = 0.0,
+                             max_df = 0.9)
+arr = []
+def chunkIterator(df):
+    for data_point in df['reviewText'].values.astype('U'):
+        yield data_point
+#arr = vectorizer.fit_transform(chunkIterator(train))
+train_vectors = vectorizer.fit_transform(train['reviewText'].values.astype('U'))
+test_vectors = vectorizer.transform(test['reviewText'].values.astype('U'))
+
+# Perform classification with SVM, kernel=linear
+classifier_linear = svm.SVC(kernel='linear')
+t0 = time.time()
+classifier_linear.fit(train_vectors, train['Label'])
+t1 = time.time()
+prediction_linear = classifier_linear.predict(test_vectors)
+t2 = time.time()
+time_linear_train = t1-t0
+time_linear_predict = t2-t1
+# results
+print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
+report = classification_report(test['Label'], prediction_linear, output_dict=True)
+
+review = """SUPERB, I AM IN LOVE IN THIS PHONE"""
 review_vector = vectorizer.transform([review]) # vectorizing
 print(classifier_linear.predict(review_vector))
 
+review = """Do not purchase this product. My cell phone blast when I switched the charger"""
+review_vector = vectorizer.transform([review]) # vectorizing
+print(classifier_linear.predict(review_vector))
+
+review = """This product doesn't work correctly"""
+review_vector = vectorizer.transform([review]) # vectorizing
+print(classifier_linear.predict(review_vector))
+
+#rcParams['figure.figsize'] = 10,5
+plot_confusion_matrix(prediction_linear,test['Label'])
+acc_score = accuracy_score(prediction_linear,test['Label'])
+pre_score = precision_score(prediction_linear,test['Label'], average=None)
+rec_score = recall_score(prediction_linear,test['Label'],average=None)
+f1score = f1_score(prediction_linear,test['Label'],average=None)
+print('Accuracy_score: ',acc_score)
+print('Precision_score: ',pre_score)
+print('Recall_score: ',rec_score)
+print('F1 score', f1score)
+print("-"*50)
+cr = classification_report(prediction_linear,test['Label'])
+print(cr)
+
+# Hashing vectorizer
+vectorizer = HashingVectorizer()
+arr = []
+def chunkIterator(df):
+    for data_point in df['reviewText'].values.astype('U'):
+        yield data_point
+#arr = vectorizer.fit_transform(chunkIterator(train))
+train_vectors = vectorizer.fit_transform(train['reviewText'].values.astype('U'))
+test_vectors = vectorizer.transform(test['reviewText'].values.astype('U'))
+
+# Perform classification with SVM, kernel=linear
+classifier_linear = svm.SVC(kernel='linear')
+t0 = time.time()
+classifier_linear.fit(train_vectors, train['Label'])
+t1 = time.time()
+prediction_linear = classifier_linear.predict(test_vectors)
+t2 = time.time()
+time_linear_train = t1-t0
+time_linear_predict = t2-t1
+# results
+print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
+report = classification_report(test['Label'], prediction_linear, output_dict=True)
+
+review = """SUPERB, I AM IN LOVE IN THIS PHONE"""
+review_vector = vectorizer.transform([review]) # vectorizing
+print(classifier_linear.predict(review_vector))
+
+review = """Do not purchase this product. My cell phone blast when I switched the charger"""
+review_vector = vectorizer.transform([review]) # vectorizing
+print(classifier_linear.predict(review_vector))
+
+review = """This product doesn't work correctly"""
+review_vector = vectorizer.transform([review]) # vectorizing
+print(classifier_linear.predict(review_vector))
+
+#rcParams['figure.figsize'] = 10,5
+plot_confusion_matrix(prediction_linear,test['Label'])
+acc_score = accuracy_score(prediction_linear,test['Label'])
+pre_score = precision_score(prediction_linear,test['Label'], average=None)
+rec_score = recall_score(prediction_linear,test['Label'],average=None)
+f1score = f1_score(prediction_linear,test['Label'],average=None)
+print('Accuracy_score: ',acc_score)
+print('Precision_score: ',pre_score)
+print('Recall_score: ',rec_score)
+print('F1 score', f1score)
+print("-"*50)
+cr = classification_report(prediction_linear,test['Label'])
+print(cr)
