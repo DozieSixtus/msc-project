63939ede29562b2c3cba0a902095431ac817410c
DozieSixtus <doziesixtus@gmail.com>
2024-08-10 22:20:47 +0100

    Add glove embedding

--- src/model/vectorizers.py
+++ src/model/vectorizers.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import torch
 import numpy as np
-import tensorflow as tf
+import nltk
 
 from transformers import AlbertTokenizer, AlbertModel, AutoTokenizer, AutoModel
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from nn_model import nn_model
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+# Path to GloVe embeddings file (ensure you have this file)
+glove_file_path = 'glove.6B.300d.txt'
 
 def get_tfidf_vectors(train, test):
     # Initialize TF-IDF Vectorizer
@@ -56,3 +56,37 @@ def get_hashing_vectors(train, test):
     test_vectors = vectorizer.transform(test.values.astype('U'))
     
     return train_vectors, test_vectors
+
+def get_glove_vectors(text_df, embedding_dim=300):
+    # download glove and unzip it in Notebook.
+    #!wget http://nlp.stanford.edu/data/glove.6B.zip
+    #!unzip glove*.zip
+
+    # Load pre-trained GloVe embeddings
+    def load_glove_embeddings(glove_file_path):
+        embeddings = {}
+        with open(glove_file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                vector = np.asarray(values[1:], dtype='float32')
+                embeddings[word] = vector
+        return embeddings
+
+    # Convert text to GloVe embeddings
+    def text_to_glove(text, embeddings, embedding_dim):
+        words = word_tokenize(text.lower())
+        word_embeddings = [embeddings[word] for word in words if word in embeddings]
+        
+        if len(word_embeddings) > 0:
+            word_embeddings = np.mean(word_embeddings, axis=0)
+            return word_embeddings
+        else:
+            return np.zeros(embedding_dim)
+
+    # Load GloVe embeddings
+    glove_embeddings = load_glove_embeddings(glove_file_path)
+
+    # Convert text to GloVe embeddings and prepare data
+    text_vectors = np.array([text_to_glove(text, glove_embeddings, embedding_dim) for text in text_df])
+    return text_vectors
